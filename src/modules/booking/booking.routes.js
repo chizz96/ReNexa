9f0952753a22a711744214b0184925745cfcdb31
chizz96 <chisomnwachukwu2001@gmail.com>
2026-08-23 16:40:57 +0100

    changes on booking module

--- src/modules/booking/booking.routes.js
+++ src/modules/booking/booking.routes.js
@@ -11,12 +11,12 @@ const router = Router();
 router.use(authverification);
 
 router.post( "/bookings", authorize(UserRole.HOUSEHOLD, UserRole.BUSINESS_OWNER), validate(createBookingSchema), bookingController.createBooking);
-router.patch("/:bookingId/assign", authorize(UserRole.ADMIN), validate(assignPickerSchema), bookingController.assignPickerToBooking);
-router.patch( "/:bookingId/complete", authorize(UserRole.ADMIN), validate(completeBookingSchema), bookingController.completeBooking);
-router.get( "/bookings", authorize(UserRole.HOUSEHOLD, UserRole.BUSINESS_OWNER, UserRole.ADMIN), bookingController.getBookings);
-router.get( "/bookings/:bookingId", authorize(UserRole.HOUSEHOLD, UserRole.BUSINESS_OWNER, UserRole.ADMIN), bookingController.getBookingById);  
-router.get( "/bookings/picker/:pickerId", authorize(UserRole.ADMIN), bookingController.getBookingsByPickerId);
 router.get( "/bookings/all", authorize(UserRole.ADMIN), bookingController.getAllBookings);
 router.get( "/bookings/mine", authorize(UserRole.HOUSEHOLD, UserRole.BUSINESS_OWNER), bookingController.getMyBookings);
+router.get( "/bookings/picker/:pickerId", authorize(UserRole.ADMIN), bookingController.getBookingsByPickerId);
+router.get( "/bookings", authorize(UserRole.HOUSEHOLD, UserRole.BUSINESS_OWNER, UserRole.ADMIN), bookingController.getBookings);
+router.get( "/bookings/:bookingId", authorize(UserRole.HOUSEHOLD, UserRole.BUSINESS_OWNER, UserRole.ADMIN), bookingController.getBookingById);  
+router.patch("/:bookingId/assign", authorize(UserRole.ADMIN), validate(assignPickerSchema), bookingController.assignPickerToBooking);
+router.patch( "/:bookingId/complete", authorize(UserRole.ADMIN), validate(completeBookingSchema), bookingController.completeBooking);
 
 export default router;
